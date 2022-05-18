17f0267c66865dc9bd5558a219fa1caacc16b2d9
juanan0309 <juan.quiroz@wizeline.com>
2022-05-18 07:00:15 -0500

    add env variables

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { GoogleAuthProvider, getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCiywLLtGlAUfbxuFJiNsqQw2tfZJQizVk",
-  authDomain: "clone-slack-4bacd.firebaseapp.com",
-  projectId: "clone-slack-4bacd",
-  storageBucket: "clone-slack-4bacd.appspot.com",
-  messagingSenderId: "249543588180",
-  appId: "1:249543588180:web:345c7c4e212309edcf70c1",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_APP_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
